2b301c5c81c7ef6285a06a8b4ee9069bf857f2cd
iverly <iverly.dev@gmail.com>
2020-01-29 15:14:13 +0100

    feat: add getNotesData method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('Hello world');
+import * as Browser from 'zombie';
+
+export type IData = IEu[];
+
+export interface IEu {
+    name: string;
+    modules: IModule[];
+}
+
+export interface IModule {
+    name: string;
+    exams: IExam[];
+}
+
+export interface IExam {
+    name: string;
+    note: number | string;
+}
+
+export function getNotesData(identifiant: string, password: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const browser = new Browser();
+        browser.visit(
+            'https://notes.info.iut-tlse3.fr/php/visuNotes.php',
+        ).then(async () => {
+            browser.fill('input[name=identifiant]', identifiant);
+            browser.fill('input[name=pass]', password);
+            browser.window.$('button').click();
+            browser.wait().then(() => {
+                resolve(browser.html());
+            });
+        }).catch((err) => {
+            reject(err);
+        });
+    });
+}
