9e004ebcf4e4cad93a1ac8f01ab3f677ccf62bc4
iverly <iverly.dev@gmail.com>
2020-01-29 15:43:04 +0100

    feat: refactor and add default methods without parsing

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import * as Browser from 'zombie';
+import * as HtmlParser from 'html-to-json-data';
+import { group, text } from 'html-to-json-data/definitions';
 
-export type IData = IEu[];
+type IData = IEu[];
 
-export interface IEu {
+interface IEu {
     name: string;
     modules: IModule[];
 }
 
-export interface IModule {
+interface IModule {
     name: string;
     exams: IExam[];
 }
 
-export interface IExam {
+interface IExam {
     name: string;
     note: number | string;
 }
 
-export function getNotesData(identifiant: string, password: string): Promise<string> {
+interface IError {
+    code: string;
+}
+
+function getData(identifiant: string, password: string): Promise<any> {
     return new Promise((resolve, reject) => {
         const browser = new Browser();
         browser.visit(
@@ -27,10 +33,31 @@ export function getNotesData(identifiant: string, password: string): Promise<str
             browser.fill('input[name=pass]', password);
             browser.window.$('button').click();
             browser.wait().then(() => {
-                resolve(browser.html());
+                resolve(HtmlParser(browser.html(), {
+                    definitions: group('html', {
+                        eu: text('body > ul > b'),
+                        mat: text('body > ul > ul > b'),
+                    }),
+                    notes: group('table tr', {
+                        name: text('td[style="width:60%"]'),
+                        value: text('td[style="width:40%"]'),
+                    }),
+                }));
             });
-        }).catch((err) => {
-            reject(err);
-        });
+        }).catch(reject);
+    });
+}
+
+export default function (identifiant: string, password: string): Promise<IData> {
+    return new Promise(async (resolve, reject: (reason?: IError) => void) => {
+        if (!identifiant) return reject({ code: 'IdentifiantNotFound' });
+        if (!password) return reject({ code: 'PasswordNotFound' });
+        try {
+            const data = await getData(identifiant, password);
+            if (!data.notes || data.notes.lenght === 0) return reject({ code: 'Incorrect credentials' });
+            resolve(data);
+        } catch (err) {
+            return reject({ code: 'UnknowError' });
+        }
     });
 }
